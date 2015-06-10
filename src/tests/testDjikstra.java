a3d5ec78f30ea25a695c0331beb58c57d1e0cfba
GabrielaRojas <gabriela.rojas@modelitsoftware.com>
2015-06-10 00:06:37 -0300

    Dijkstra casi funcionando

--- src/tests/testDjikstra.java
+++ src/tests/testDjikstra.java
@@ -15,34 +15,67 @@ public class testDjikstra {
 	@Test
 	public void testCaminoMasCorto(){
 		Sistema s = new Sistema();
-		s.inicializarSistema(5);
+		s.inicializarSistema(8);
 		
-		Esquina e = new Esquina(-34.9055, -56.1892);
-		Retorno ret = s.registrarEsquina(-34.9055,-56.1892);
+		Retorno ret = s.registrarEsquina(1.0,1.0);
 		assertEquals(Retorno.Resultado.OK, ret.resultado);
-		ret = s.registrarPuntoInteres(-34.9058, -56.1927, Rubro.CAJERO, "hola");
+		ret = s.registrarPuntoInteres(2.0, 2.0, Rubro.CAJERO, "hola");
 		assertEquals(Retorno.Resultado.OK, ret.resultado);
-		ret = s.registrarPuntoInteres(-34.9032, -56.2040, Rubro.CAJERO, "hola");
+		ret = s.registrarPuntoInteres(3.0, 3.0, Rubro.CAJERO, "hola");
 		assertEquals(Retorno.Resultado.OK, ret.resultado);
-		ret = s.registrarEsquina(-34.9059,-56.1929);
+		ret = s.registrarEsquina(4.0, 4.0);
 		assertEquals(Retorno.Resultado.OK, ret.resultado);
-		ret = s.registrarEsquina(-34.9039,-56.2049);
+		ret = s.registrarEsquina(5.0, 5.0);
 		assertEquals(Retorno.Resultado.OK, ret.resultado);
-		
-		ret = s.registrarTramo(-34.9055, -56.1892, -34.9058, -56.1927, 4);
+		ret = s.registrarPuntoInteres(6.0, 6.0, Rubro.CAJERO, "hola");
 		assertEquals(Retorno.Resultado.OK, ret.resultado);
-		ret = s.registrarTramo(-34.9032, -56.2040, -34.9055, -56.1892, 3);
+		ret = s.registrarEsquina(7.0, 7.0);
 		assertEquals(Retorno.Resultado.OK, ret.resultado);
-		ret = s.registrarTramo(-34.9058, -56.1927, -34.9032, -56.2040, 1);
-		assertEquals(Retorno.Resultado.OK, ret.resultado);
-		ret = s.registrarTramo(-34.9059, -56.1929, -34.9039, -56.2049, 5);
-		assertEquals(Retorno.Resultado.OK, ret.resultado);
-		ret = s.registrarTramo(-34.9058, -56.1927, -34.9039, -56.2049, 2);
+		ret = s.registrarEsquina(8.0, 8.0);
 		assertEquals(Retorno.Resultado.OK, ret.resultado);
 		
-		int keyOrig = s.tableHash.h(e.getCoordX(), e.getCoordY());
+		ret = s.registrarTramo(1.0, 1.0, 2.0, 2.0, 2);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		ret = s.registrarTramo(1.0, 1.0, 4.0, 4.0, 14);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		
+		ret = s.registrarTramo(2.0, 2.0, 3.0, 3.0, 1);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		ret = s.registrarTramo(2.0, 2.0, 6.0, 6.0, 2);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		ret = s.registrarTramo(2.0, 2.0, 4.0, 4.0, 4);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		ret = s.registrarTramo(2.0, 2.0, 5.0, 5.0, 9);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		
+		ret = s.registrarTramo(3.0, 3.0, 6.0, 6.0, 4);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		
+		ret = s.registrarTramo(4.0, 4.0, 7.0, 7.0, 6);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		ret = s.registrarTramo(4.0, 4.0, 5.0, 5.0, 1);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		
+		ret = s.registrarTramo(5.0, 5.0, 6.0, 6.0, 1);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		ret = s.registrarTramo(5.0, 5.0, 8.0, 8.0, 1);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		ret = s.registrarTramo(5.0, 5.0, 7.0, 7.0, 3);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		
+		ret = s.registrarTramo(6.0, 6.0, 8.0, 8.0, 3);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		
+		ret = s.registrarTramo(7.0, 7.0, 8.0, 8.0, 1);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		
+		int keyOrig = s.tableHash.devolverPosActual(7.0,7.0);
+		int keyFin = s.tableHash.devolverPosActual(1.0, 1.0);
+		
+		s.tableHash.imprimirLista();
+		
 		Dijkstra d = new Dijkstra();
-		d.dijkstra(s.matrizMapa, keyOrig);
+		d.dijkstra(s.matrizMapa, keyOrig, keyFin);
 		d.imprimirCamino(s.matrizMapa);
 	}
 }
