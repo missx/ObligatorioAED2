9b0f8276c2f0f7d231c6b3900cd23bba054ff940
GabrielaRojas <gabriela.rojas@modelitsoftware.com>
2015-06-14 00:36:19 -0300

    Volvi a agregar el test2 de dijkstra

--- src/tests/testDjikstra.java
+++ src/tests/testDjikstra.java
@@ -77,6 +77,55 @@ public class testDjikstra {
 		Dijkstra d = new Dijkstra();
 		d.dijkstra(s.matrizMapa, keyOrig, keyFin);
 		d.imprimirCamino(s.matrizMapa);
+		System.out.println(" ");
+	}
+	
+	@Test
+	public void testCaminoMasCorto2(){
+		Sistema s = new Sistema();
+		s.inicializarSistema(8);
+	
+		Retorno ret = s.registrarEsquina(1.0,1.0);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		ret = s.registrarPuntoInteres(2.0, 2.0, Rubro.CAJERO, "hola");
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		ret = s.registrarPuntoInteres(3.0, 3.0, Rubro.CAJERO, "hola");
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		ret = s.registrarEsquina(4.0, 4.0);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		ret = s.registrarEsquina(5.0, 5.0);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		ret = s.registrarPuntoInteres(6.0, 6.0, Rubro.CAJERO, "hola");
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+
+		ret = s.registrarTramo(1.0, 1.0, 2.0, 2.0, 1);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		ret = s.registrarTramo(1.0, 1.0, 5.0, 5.0, 3);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		
+		ret = s.registrarTramo(2.0, 2.0, 4.0, 4.0, 6);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		ret = s.registrarTramo(2.0, 2.0, 5.0, 5.0, 1);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		
+		ret = s.registrarTramo(3.0, 3.0, 4.0, 4.0, 2);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		
+		ret = s.registrarTramo(4.0, 4.0, 6.0, 6.0, 4);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+		
+		ret = s.registrarTramo(5.0, 5.0, 6.0, 6.0, 7);
+		assertEquals(Retorno.Resultado.OK, ret.resultado);
+				
+		int keyOrig = s.tableHash.devolverPosActual(1.0,1.0);
+		int keyFin = s.tableHash.devolverPosActual(6.0, 6.0);
+		
+		s.tableHash.imprimirLista();
+		
+		Dijkstra d = new Dijkstra();
+		d.dijkstra(s.matrizMapa, keyOrig, keyFin);
+		d.imprimirCamino(s.matrizMapa);
+		System.out.println(" ");
 	}
 	
 }
