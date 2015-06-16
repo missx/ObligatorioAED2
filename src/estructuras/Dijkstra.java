a4135d768d362340aae53464b2112277a16f48fe
Ximena Lasserre <ximena.lasserre@modelitsoftware.com>
2015-06-16 00:32:39 -0300

    Intenté arreglar caminoMin sigue tomando mal el camino :@

--- src/estructuras/Dijkstra.java
+++ src/estructuras/Dijkstra.java
@@ -1,5 +1,7 @@
 package estructuras;
 
+import dominio.PuntoDeInteres;
+
 public class Dijkstra {
 
 	public static int[] dist;
@@ -37,19 +39,21 @@ public class Dijkstra {
 		while(!queue.isEmpty()){
 			//saco el del frente
 			int puntoActual = (int)((NodoLista)queue.dequeue()).getDato();
-			System.out.println(puntoActual);
+			System.out.println("padre " + puntoActual);
 			//boolean flag = true;
 			//Busco vertices adyacentes del origen
 			Lista l = obtenerAdyacentes(puntoActual);
 			System.out.println("es vacia " + l.esVacia());
 			
-			NodoLista nodo = l.getInicio();
+			//NodoLista nodo = l.getInicio();
+			NodoLista nodo = obtenerNodoMenorDistancia(l, puntoActual);
 			while(nodo != null){
 				int otroPunto = (int)nodo.getDato();
-				System.out.println(otroPunto);
+				l.borrarUnElem(otroPunto);
+				System.out.println("mostrar hijo " + otroPunto);
 				int peso = grafo.devolverDistancia(otroPunto, puntoActual);
 				
-				if(!visited[otroPunto] || dist[puntoActual] + peso < dist[puntoActual]){
+				if(!visited[otroPunto] || dist[puntoActual] + peso < dist[otroPunto]){
 					visited[otroPunto] = true;
 					prec[otroPunto] = puntoActual;
 					dist[otroPunto] = dist[puntoActual] + peso;
@@ -57,7 +61,7 @@ public class Dijkstra {
 					//enqueue punto
 					queue.enqueue(otroPunto);
 				}
-				nodo = nodo.getSig();
+				nodo = obtenerNodoMenorDistancia(l, puntoActual);
 			}
 			
 			//Obtengo el vertice de menor distancia
@@ -75,14 +79,48 @@ public class Dijkstra {
 			if(flag){
 				System.out.println(vmd);
 				dijkstra(vmd);
-			}*/
+			}
+		}*/
 		}
-		
 	}
 
 	private Lista obtenerAdyacentes(int punto){
 		return grafo.obtenerVerticesAdyacentes(punto);
 	}
+	
+	private NodoLista obtenerNodoMenorDistancia(Lista l, int actual){
+		NodoLista verticeMenor = null;
+		int vertice = 0;
+		int pesoAux = 0;
+		int menorPeso = Integer.MAX_VALUE;
+		NodoLista aux = l.getInicio();
+		int x = Integer.MAX_VALUE;
+		int y = -1;   // graph not connected, or no unvisited vertices
+		while(aux != null){
+			vertice = (Integer)aux.getDato();
+			//Verificar si el punto ya fue visitado
+			if(!visited[vertice]){
+				prec[vertice] = actual;
+				pesoAux = grafo.devolverDistancia(vertice, actual);
+				if(dist[actual] + pesoAux > dist[vertice]){
+					return aux;
+				}
+				dist[vertice] = dist[actual] + pesoAux;
+				if(pesoAux < menorPeso){
+					menorPeso = pesoAux;
+					verticeMenor = aux;
+				}
+				if(pesoAux == menorPeso){
+					//algo
+				}
+				aux = aux.getSig();
+			}
+			else{
+				aux = aux.getSig();
+			}
+		}
+		return verticeMenor;
+	}	
 	
 	private int obtenerVerticeMenorDistancia(Lista l, int actual){
 		int verticeMenor = 0;
